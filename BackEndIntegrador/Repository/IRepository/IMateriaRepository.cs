0a41b992f81bb6254b9d691a5bd0450282e83fad
Brayan <bcardenas656@gmail.com>
2021-10-15 10:55:54 -0500

    cambios en IMateriaRepositori y añadir MateriaRepository

--- BackEndIntegrador/Repository/IRepository/IMateriaRepository.cs
+++ BackEndIntegrador/Repository/IRepository/IMateriaRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BackEndIntegrador.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,20 @@ namespace BackEndIntegrador.Repository.IRepository
 {
     public interface IMateriaRepository
     {
+        ICollection<Materia> getMateria();
+
+        Materia getMateria(int Id_materia);
+
+        bool existeMateria(string Materia);
+
+        bool existeMateria(int Id_materia);
+
+        bool crearMateria(Materia materia);
+
+        bool actualizarMateria(Materia materia);
+
+        bool borrarMateria(Materia materia);
+
+        bool Guardar();
     }
 }
